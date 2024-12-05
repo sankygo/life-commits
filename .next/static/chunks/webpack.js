ecca971c6508947dda96394b811d83637fedd181
Sankalpana Agarwal <sankalpana@Sankalpanas-MacBook-Pro.local>
2024-12-06 00:06:34 +0530

    Add ESLint config

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("45590dfb3ceea700")
+/******/ 		__webpack_require__.h = () => ("1743a4739a1c5805")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
