b3e456ded9ab3800162d85ade45b0dea26395f40
Sankalpana Agarwal <sankalpana@Sankalpanas-MacBook-Pro.local>
2024-12-06 00:23:44 +0530

    Add ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptEslintParser from '@typescript-eslint/parser';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    //extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: typescriptEslintParser, // Use TypeScript parser
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      '@typescript-eslint': typescriptEslintPlugin,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      ...typescriptEslintPlugin.configs.recommended.rules,
-      //...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default {
+  // Specify the root directory to avoid conflicts with nested ESLint configs
+  root: true,
+
+  // Extend the base ESLint configurations
+  extends: [
+    js.configs.recommended, // Base JS rules
+    'plugin:@typescript-eslint/recommended', // TypeScript ESLint plugin rules
+    'plugin:react-hooks/recommended', // React Hooks plugin
+    'plugin:react-refresh/recommended', // React Refresh plugin
+  ],
+
+  // Define the parser for TypeScript files
+  parser: typescriptEslintParser,
+  
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module', // For ES Modules
   },
-)
+
+  // Define the environment (Global variables)
+  env: {
+    browser: true, // If your code runs in the browser environment
+  },
+
+  // Plugins used in your config
+  plugins: [
+    'react-hooks', 
+    'react-refresh',
+    '@typescript-eslint',
+  ],
+
+  // Define custom rules or override existing ones
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn', 
+      { allowConstantExport: true },
+    ],
+    // Add other custom rules as needed
+  },
+
+  // Files to lint
+  files: ['**/*.{ts,tsx}'],
+};
